8c72261f6558f3fea744531dc1015f5ea58e86d2
Jung Dong Wook <sasha.chun.2003@mail.ru>
2024-01-02 07:14:37 +0300

    structured request and functional method

--- notes.h
+++ notes.h
@@ -2,12 +2,23 @@
 #include <cstdlib>
 #include "libpq-fe.h"
 
-//Data for generation connection request
+// Data for generation connection request
 struct DbConnectParameters {
     const char* username;
     const char* password;
     std::string host;
 };
+
+// Data for adding note
+// todo: еще добавить столбцов
+struct InfoToInsert {
+    std::string task;
+};
+
+// todo: сделать изменение порядкового номера
+// struct InfoToDelete {
+//     std::string 
+// }
 
 enum class QueryType {
     INSERT, 
@@ -20,20 +31,13 @@ class DatabaseManager {
 private: 
     PGconn* conn;
 
-public:
-    //Connection request forming
+    // Connection request forming
     std::string GetFullRequest(DbConnectParameters& parameters){
         std::string returnableQuery = "dbname=notes user=" + std::string(parameters.username) + " password=" + std::string(parameters.password) + parameters.host;
         return returnableQuery;
     }
 
-    void ConnectToServer(DbConnectParameters& requestParameters){
-        std::string request = GetFullRequest(requestParameters);
-
-        // Connection
-        conn = PQconnectdb(request.c_str());
-    }
-
+    //todo: сделать разделение на операции по QueryType
     void SendQueryToServer(QueryType type, const char* query){
         PGresult *result = PQexec(conn, query);
 
@@ -45,6 +49,20 @@ public:
         } else {
             std::cerr << "Не удалось выполнить SQL-запрос: " << PQerrorMessage(conn) << std::endl;
         }
+    }
+
+public:
+    void ConnectToServer(DbConnectParameters& requestParameters){
+        std::string request = GetFullRequest(requestParameters);
+
+        // Connection
+        conn = PQconnectdb(request.c_str());
+    }
+
+    void AddTask(std::string& task){
+        std::string queryStr = "INSERT INTO notes(note) VALUES ('" + task + "');";
+        const char* query = queryStr.c_str();
+        SendQueryToServer(QueryType::INSERT, query);
     }
 
     void StopConnection(){
